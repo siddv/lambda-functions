6d84b45c85abf86e9ca9a7e520b1f212fffa6d51
Siddharth Vadgama <me@siddv.net>
2018-07-21 08:33:43 +0100

    init vuepress with i18n support

---
+++ src/docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'English', // this will be set as the lang attribute on <html>
+      title: `Sidd's Lambda Functions`,
+      description: 'A collection of Lambda Functions built by Sidd'
+    },
+    '/ca/': {
+      lang: 'Catalan',
+      title: 'Funcions Lambda del Sidd',
+      description: 'Una col · lecció de Funcions Lambda construïda per Sidd'
+    }
+  }
+}
