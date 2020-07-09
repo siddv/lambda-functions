3b4c275b8c20a84d46a93726ae2caa576f477daf
siddv <me@siddv.net>
2020-07-09 14:17:56 +0100

    Add Initial tech function

---
+++ src/functions/tech.js
@@ -0,0 +1,19 @@
+exports.handler = async (event, context) => {
+    // let response = ':sausage-roll-left::sausage-roll-middle::sausage-roll-right:'
+  
+    // if(event && event.queryStringParameters && event.queryStringParameters.text) {
+    //   response = `:sausage-roll-top:\n${(':sausage-roll-middle-vertical:\n'.repeat(parseInt(event.queryStringParameters.text)))}:sausage-roll-bottom:`;
+    // }
+  
+    return {
+      headers: {
+        "content-type": "application/json"
+      },
+      statusCode: 200,
+      body: JSON.stringify({
+        response_type: 'in_channel',
+        text: 'hello world'
+      })
+    };
+  
+  }
